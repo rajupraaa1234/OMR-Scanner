652d385c417d7228e6a36d50600c5ab6b586bc17
rajukumar <rajupraaa1234@gmail.com>
2022-03-09 10:42:05 +0530

    first commit

---
+++ Utils.py
@@ -0,0 +1,106 @@
+from tkinter.messagebox import QUESTION
+from traceback import print_tb
+import cv2
+from cv2 import contourArea
+import numpy as np
+
+#############################
+question = 5
+choice = 5
+#############################
+
+## TO STACK ALL THE IMAGES IN ONE WINDOW
+def stackImages(imgArray,scale,lables=[]):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        hor_con= np.concatenate(imgArray)
+        ver = hor
+    if len(lables) != 0:
+        eachImgWidth= int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        #print(eachImgHeight)
+        for d in range(0, rows):
+            for c in range (0,cols):
+                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
+                cv2.putText(ver,lables[d][c],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
+    return ver
+
+
+def rectContous(contours):
+    rectCon=[]
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area>50:
+            peri = cv2.arcLength(i,True)
+            approx = cv2.approxPolyDP(i,0.02*peri,True)    # giving me 4 points of coordinate
+            if len(approx)==4:
+                rectCon.append(i)
+    rectCon = sorted(rectCon,key=cv2.contourArea,reverse=True)
+    return rectCon 
+        
+def getCornorPoints(cont):
+    peri = cv2.arcLength(cont,True)
+    approx = cv2.approxPolyDP(cont,0.02*peri,True)
+    return approx
+
+
+# found all the 4 points of largest Contours 
+#
+# [[230  5]
+# [231 638]
+# [432 637]
+# [424  12]]
+
+# [ 235  869 1069  436] got the array of sum (x+y) => (230 + 5) => 235 where minimum sum is denoted the origin 
+#
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2)) # REMOVE EXTRA BRACKET
+    print(myPoints)
+    myPointsNew = np.zeros((4, 1, 2), np.int32) # NEW MATRIX WITH ARRANGED POINTS
+    add = myPoints.sum(1)
+    #print(add)
+    #print(np.argmax(add))
+    myPointsNew[0] = myPoints[np.argmin(add)]  #[0,0]
+    myPointsNew[3] =myPoints[np.argmax(add)]   #[w,h]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] =myPoints[np.argmin(diff)]  #[w,0]
+    myPointsNew[2] = myPoints[np.argmax(diff)] #[h,0]  
+
+    return myPointsNew
+
+# Sp
+def splitBox(img):
+    rows = np.vsplit(img,question)   
+    boxes = []
+    for r in rows:
+        cols = np.hsplit(r,choice)
+        for box in cols:
+            boxes.append(box)
+            cv2.imshow("Splite",box)
+
+    return boxes
+
+
+    
+
