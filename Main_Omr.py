e7767dc70dc31efe1721236b122c39b0e674b7ee
Raju kumar <48593134+rajupraaa1234@users.noreply.github.com>
2022-03-11 18:32:27 +0530

    Update Main_Omr.py

--- Main_Omr.py
+++ Main_Omr.py
@@ -1,94 +1,73 @@
+from asyncio.windows_events import NULL
 from importlib.resources import path
+from pickle import FALSE, TRUE
 from traceback import print_tb
 import cv2
 from cv2 import THRESH_BINARY_INV
 import numpy as np
 import Utils
+import json
 #####################################################
-path = "4.png"
-widhImg =500
-heightImg = 500
-question = 10
+path = "da.JPG"
+widhImg = 450
+heightImg =450
+question = 14
 choice = 4
 #####################################################
 # Declare the Answer for 1 to 10 Question 
-ans = [0,2,3,3,1,0,2,3,1,3]
+ans = [
+       [0,2,3,3,1,0,2,3,1,3,0,1,2,3],
+       [0,2,3,3,1,0,2,3,1,3,0,1,2,3],
+       [0,2,3,3,1,0,2,3,1,3,0,1,2,3], 
+      ]
 #####################################################
 
 # PREPROCESSING
-img=cv2.imread(path)
+img=Utils.ExtractInnerImage(FALSE)
+cv2.imshow("ExtractImage" , img)
+
+#img=cv2.imread(path)
 img = cv2.resize(img,(widhImg,heightImg))
 imgContours = img.copy()
 imgBiggestContours = img.copy()
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
-imgCanny = cv2.Canny(imgBlur,40,80)    # fining all edges by using Canny method
+imgCanny = cv2.Canny(imgBlur,30,70)    # fining all edges by using Canny method
+
 
 #Finding All Contours
 countours , hirerarchy = cv2.findContours(imgCanny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 cv2.drawContours(imgContours,countours,-1,(0,255,0),4)
 
-
 #find Rectangle
 rectCon = Utils.rectContous(countours)
-print("size--->",rectCon)
-biggestCont = Utils.getCornorPoints(rectCon[0])
+AllMCQBoxes = [2,0,1]
+count = 0
+FinalGrade = 0
+UnAttempted = 0
+Invaide = 0
 
-print("shape -----------> ",biggestCont.shape)
+for i in AllMCQBoxes:
+    biggestCont = Utils.getCornorPoints(rectCon[i])
+    CurrMcqBox = Utils.getAttemtedQuestionList(biggestCont,imgBiggestContours,img,ans[count])
+    print("CurrMcqBox",CurrMcqBox)
+    for j in range(0,question):
+        if ans[count][j] == CurrMcqBox[j]:
+            FinalGrade +=1
+        elif CurrMcqBox[j] == choice+1:
+            Invaide +=1
+        elif CurrMcqBox[j] == choice+2:
+            UnAttempted +=1     
+    count +=1
 
-if biggestCont.size != 0:
-    cv2.drawContours(imgBiggestContours,biggestCont,-1,(0,255,0),10)
-    biggestCont = Utils.reorder(biggestCont)
-    pt1 = np.float32(biggestCont) #prepare for Wrap
-    pts2 = np.float32([[0, 0],[widhImg, 0], [0, heightImg],[widhImg, heightImg]]) # PREPARE POINTS FOR WARP
-    matrix = cv2.getPerspectiveTransform(pt1,pts2) #Get Transformation Matrix
-    imgWrapColored = cv2.warpPerspective(img,matrix,(widhImg,heightImg))
-    cv2.imshow("question",imgWrapColored)
-
-    #Apply Thershold (for detect which option has been marked )
-    imgWrapGray = cv2.cvtColor(imgWrapColored,cv2.COLOR_BGR2GRAY)
-    imgThersh = cv2.threshold(imgWrapGray,100,225,THRESH_BINARY_INV)[1]
-
-    # getting all boxes merked and un marked both in linear list
-    boxes = Utils.splitBox(imgThersh)
-    #countNonZero => The function returns the number of non-zero elements in src (like Pixel value)
-    # print(cv2,cv2.countNonZero(boxes[1]).countNonZero(boxes[2]))  # for marked options always have maximum pixel value than unmarked options
-
-    #Getting No Zero Pixel value of each box
-    myPixelVal = np.zeros((question,choice)) # creatting 2D array of same of total question
-    countC = 0
-    countR = 0
-    for image in boxes:
-        totalPixels = cv2.countNonZero(image)
-        myPixelVal[countR][countC] = totalPixels
-        countC +=1
-        if(countC == choice):
-            countR +=1
-            countC = 0
-    print(myPixelVal) 
-
-    #Now I am finding all the marked column nnumber w.r.t Question and appended in myIndex arr
-    myIndex = []
-    for x in range(0,question):
-        arr = myPixelVal[x]
-        myIndexVal = np.where(arr == np.amax(arr))
-        myIndex.append(myIndexVal[0][0])
-    print(myIndex)
-
-   # now finding Score     
-    score = 0
-    for i in range(0,question):
-        if ans[i] == myIndex[i]:
-            score +=1
-
-    print("Final Score is : ",score)        
-
+def getResult():
+    JsonRes = {
+        "Result": FinalGrade,
+        "Not_Attempted" : UnAttempted,
+        "Invalid" : Invaide
+    }
+    return json.dumps(JsonRes)
    
-    imgBlack = np.zeros_like(img) 
-    imageArray = ([img,imgGray,imgBlur,imgCanny],[imgContours,imgBiggestContours,imgWrapColored,imgThersh] )
-    imgStacked = Utils.stackImages(imageArray,0.5)
-    cv2.imshow("imgStacked",imgStacked)
-    cv2.waitKey(0)
-
+print("Final Grade",getResult())        
 
 
