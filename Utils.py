0f87897f176ca3c8ad259edf514549a50006d166
Raju kumar <48593134+rajupraaa1234@users.noreply.github.com>
2022-03-11 08:23:18 +0530

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,12 +1,16 @@
+from pickle import FALSE, TRUE
 from tkinter.messagebox import QUESTION
 from traceback import print_tb
 import cv2
-from cv2 import contourArea
+from cv2 import THRESH_BINARY_INV
 import numpy as np
 
-#############################
+############################
 question = 10
 choice = 4
+path = "full_omr.png"
+widhImg = 1100
+heightImg = 1100
 #############################
 
 ## TO STACK ALL THE IMAGES IN ONE WINDOW
@@ -80,7 +84,7 @@ def getCornorPoints(cont):
 #
 def reorder(myPoints):
     myPoints = myPoints.reshape((4, 2)) # REMOVE EXTRA BRACKET
-    print(myPoints)
+    #print(myPoints)
     myPointsNew = np.zeros((4, 1, 2), np.int32) # NEW MATRIX WITH ARRANGED POINTS
     add = myPoints.sum(1)
     #print(add)
@@ -99,15 +103,107 @@ def splitBox(img):
     rows = np.vsplit(img,question)   
     boxes = []
     for r in rows:
-        cols = np.hsplit(r,choice)
+        cols = np.hsplit(r,choice+1)
+        flag = FALSE
         for box in cols:
+            if flag == FALSE : 
+                flag = TRUE
+                continue
             boxes.append(box)
             if(count<=4):
-                cv2.imshow("Splite",box)
+                #cv2.imshow("Splite",box)
                 count +=1
 
     return boxes
 
 
-    
+def ExtractInnerImage():
+    img=cv2.imread(path)
+    img = cv2.resize(img,(widhImg,heightImg))
+    imgContours = img.copy()
+    imgBiggestContours = img.copy()
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
+    imgCanny = cv2.Canny(imgBlur,40,80)
+
+    countours , hirerarchy = cv2.findContours(imgCanny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(imgContours,countours,-1,(0,255,0),4)
+    rectCon = rectContous(countours)
+    biggestCont = getCornorPoints(rectCon[0])
+
+    if biggestCont.size != 0:
+        cv2.drawContours(imgBiggestContours,biggestCont,-1,(0,255,0),10)
+        biggestCont = reorder(biggestCont)
+        pt1 = np.float32(biggestCont) #prepare for Wrap
+        pts2 = np.float32([[0, 0],[widhImg, 0], [0, heightImg],[widhImg, heightImg]]) # PREPARE POINTS FOR WARP
+        matrix = cv2.getPerspectiveTransform(pt1,pts2) #Get Transformation Matrix
+        imgWrapColored = cv2.warpPerspective(img,matrix,(widhImg,heightImg))
+        #cv2.imshow("question from method",imgWrapColored)
+        return imgWrapColored
+
+
+def getAttemtedQuestionList(biggestCont,imgBiggestContours,img,ans):
+    if biggestCont.size != 0:
+        cv2.drawContours(imgBiggestContours,biggestCont,-1,(0,255,0),10)
+        biggestCont = reorder(biggestCont)
+        pt1 = np.float32(biggestCont) #prepare for Wrap
+        pts2 = np.float32([[0, 0],[widhImg, 0], [0, heightImg],[widhImg, heightImg]]) # PREPARE POINTS FOR WARP
+        matrix = cv2.getPerspectiveTransform(pt1,pts2) #Get Transformation Matrix
+        imgWrapColored = cv2.warpPerspective(img,matrix,(widhImg,heightImg))
+        # cv2.imshow("question",imgWrapColored)
+        # cv2.waitKey(0)
+        
+        #Apply Thershold (for detect which option has been marked )
+        imgWrapGray = cv2.cvtColor(imgWrapColored,cv2.COLOR_BGR2GRAY)
+        imgThersh = cv2.threshold(imgWrapGray,90,225,THRESH_BINARY_INV)[1]
+
+        # getting all boxes merked and un marked both in linear list
+        boxes = splitBox(imgThersh)
+        #countNonZero => The function returns the number of non-zero elements in src (like Pixel value)
+        # print(cv2,cv2.countNonZero(boxes[1]).countNonZero(boxes[2]))  # for marked options always have maximum pixel value than unmarked options
+
+        #Getting No Zero Pixel value of each box
+        myPixelVal = np.zeros((question,choice)) # creatting 2D array of same of total question
+        countC = 0
+        countR = 0
+        for image in boxes:
+            totalPixels = cv2.countNonZero(image)
+            myPixelVal[countR][countC] = totalPixels
+            countC +=1
+            if(countC == choice):
+                countR +=1
+                countC = 0
+        print(myPixelVal) 
+
+        #Now I am finding all the marked column nnumber w.r.t Question and appended in myIndex arr
+        myIndex = []
+        for x in range(0,question):
+            arr = myPixelVal[x]
+            myIndexVal = np.where(arr == np.amax(arr))
+            myIndex.append(myIndexVal[0][0])
+        print(myIndex)
+
+        # now finding Score     
+        score = 0
+        for i in range(0,question):
+            if ans[i] == myIndex[i]:
+                score +=1
+
+        print("Score for current box : ",score)  
+
+        return myIndex      
+
+   
+        # imgBlack = np.zeros_like(img) 
+        # imageArray = ([img,imgGray,imgBlur,imgCanny],[imgContours,imgBiggestContours,imgWrapColored,imgThersh] )
+        # cv2.imshow("imgContours",imgContours)
+        # cv2.imshow("imgBiggestContours",imgBiggestContours)
+        # cv2.imshow("imgThersh",imgThersh)
+        # imgStacked = stackImages(imageArray,0.5)
+        # cv2.imshow("imgStacked",imgStacked)
+        # cv2.waitKey(0)
+
+
+
+
 
